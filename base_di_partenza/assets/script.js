614ef198af317ffca469b043b37a4224bff1e805
Fabio Gilardi <fabiogilardi02@gmail.com>
2023-12-22 16:42:35 +0100

    animation started

--- base_di_partenza/assets/script.js
+++ base_di_partenza/assets/script.js
@@ -1,11 +1,11 @@
 window.onscroll = function () {
   const greenCircle = document.getElementById("circle");
-  const header = document.getElementsByTagName("header")[0]; // Replace 'myHeader' with your header's ID
+  const header = document.getElementsByTagName("header")[0];
   if (window.scrollY > 400) {
     greenCircle.style.backgroundColor = "#1a8917";
-    header.classList.add("scrolled"); // Add a class name for scrolled state
+    header.classList.add("scrolled");
   } else {
     greenCircle.style.backgroundColor = "#191919";
-    header.classList.remove("scrolled"); // Remove the class when not scrolled
+    header.classList.remove("scrolled");
   }
 };
